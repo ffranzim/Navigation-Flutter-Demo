dfc0a5633f07786ba24f89aaef8d3d0ce081cbea
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-23 13:52:00 -0300

    Secao 12 - Aula 142 - Rotas Basico

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "navigation_flutter_demo";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
