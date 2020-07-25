757a054a16127ed80fbcb3e1ab6859fd8925e9c3
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-25 15:04:19 -0300

    Adicionando o ciclo de vida da widget StatefulWidget

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  void initState() {
+    super.initState();
+    print('InitState()');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build()');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -19,4 +31,30 @@ class Screen2 extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  void deactivate() {
+    print('deactivate()');
+  }
 }
+
+//class Screen2 extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        backgroundColor: Colors.blue,
+//        title: Text('Screen 2'),
+//      ),
+//      body: Center(
+//        child: RaisedButton(
+//          color: Colors.blue,
+//          child: Text('Go Back To Screen 1'),
+//          onPressed: () {
+//            Navigator.pop(context);
+//          },
+//        ),
+//      ),
+//    );
+//  }
+//}
